01496b0fcbf57fa775fc7f9208a9871564e528a2
embeddededucation <iot.education14@gmail.com>
2026-06-07 16:26:44 +0530

    Update gpio.c
    All GPIO functions and operations are put in this file

--- 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/gpio.c
+++ 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/gpio.c
@@ -1,1 +1,31 @@
+/*
+ * gpio.c
+ *
+ * Created: 05/06/2026 09:28:45
+ *  Author: Anand
+ */ 
 
+#include "config.h"
+#include <avr/io.h>   // ensures PORTB, DDRB, PB5, etc. are known
+#include "gpio.h"
+
+
+
+
+void toggleLED()
+{
+	PORTB ^= _BV(LED_PIN);
+}
+
+void setPB5OutPB7Input()
+{
+	// Pin B5 as o/p (Set to 1), Internal LED
+	DDRB |= _BV(LED_PIN);
+
+	// Pin B7 as i/p (Reset Bit),
+	// Internal button, already has pull-up on board
+	DDRB &= ~_BV(BUTTON_PIN);
+
+	// Set PB5 LED On
+	PORTB |= _BV(LED_PIN);
+}
