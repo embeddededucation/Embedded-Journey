8b3f4c4c3d8524397c85e1fecf81fa27b70358da
embeddededucation <iot.education14@gmail.com>
2026-06-07 16:30:07 +0530

    Update timer.h

--- 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/timer.h
+++ 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/timer.h
@@ -1,1 +1,15 @@
+/*
+ * timer.h
+ *
+ * Created: 05/06/2026 21:03:52
+ *  Author: Anand
+ */ 
 
+
+#ifndef TIMER_H
+#define TIMER_H
+
+void delay50msec();
+void timerCTCsetup();
+
+#endif /* TIMER_H */
