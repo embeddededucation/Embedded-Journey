d3ea3f55a170ec73ca249a18fbae3592ddd7c854
embeddededucation <iot.education14@gmail.com>
2026-06-07 16:28:12 +0530

    Update gpio.h

--- 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/gpio.h
+++ 03_ATmega168PB/Module-2_Timers/Level-2_Timer0_CTC_Mode/gpio.h
@@ -1,1 +1,20 @@
+/*
+ * gpio.h
+ *
+ * Created: 05/06/2026 09:25:41
+ *  Author: anand
+ */ 
 
+
+#ifndef GPIO_H
+#define GPIO_H
+
+// Added for any future dependencies related to AVR
+#include <avr/io.h> 
+
+void toggleLED();
+void setPB5OutPB7Input();
+
+
+
+#endif /* GPIO_H */
